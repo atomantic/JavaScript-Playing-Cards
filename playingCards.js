12a85a5ba662f3059dd7da5c9b6c48f01a1939bf
Adam Eivy <atomantic@gmail.com>
2010-06-04 13:41:53 +0200

    completely new foundation. Although allowed to be a jquery plugin, it no longer requires jquery--but allows a spread demonstration if jquery is available

--- playingCards.js
+++ playingCards.js
@@ -1,62 +1,85 @@
 /**
- * jQuery Plugin to create a deck of cards.
- * This can be used to play standard card games (which could be additional rule modules attached to this set)
+ * playingCards is a standard card deck library
+ * This can be used to play standard card games (which will be additional rule modules attached to this set)
  * 
- * usage: $("#cardTable").playingCards();
+ * usage: var cardDeck = new playingCards(conf);
  * override defaults for playingCards() and for card() with
- *    $.playingCards.defaults
- *    $.playingCards.card.defaults
+ *    playingCards.defaults
+ *    playingCards.card.defaults
  * 
  * @author Adam Eivy (antic | atomantic)
  * @license MIT (do what you like but keep the license)
  */
+
+if (Array.indexOf === undefined) {
+    // doens't exist in IE
+    /* Finds the index of the first occurence of item in the array, or -1 if not found */
+    Array.prototype.indexOf = function(item) {
+        for (var i = 0; i < this.length; ++i) {
+            if (this[i] === item) {
+                return i;
+            }
+        }
+        return - 1;
+    };
+}
+
  (function($) {
 
-    $.fn.playingCards = function(conf) {
-        var o = $.extend({},
-        $.fn.playingCards.defaults, conf);
-
+    var playingCards = window.playingCards = function(conf) {	
+        var c = objExtend(playingCards.defaults, conf);
+        if (! (this instanceof arguments.callee)) {
+			c.el = $(this);
+            return new arguments.callee(c);
+        }
+		this.conf = c;
+		this.lib = $;
+        this.init();
+		if(this.conf.startShuffled){
+			this.shuffle(5);
+		}
+		this.spread();
+		return this;
+    };
+    if ($.fn) {
+        // we can use library methods
+		// attach this as an extension to the library
+        $.fn.playingCards = playingCards;
+    }
+    playingCards.prototype.init = function() {
+        var o = this.conf;
         this.cards = [];
-		var l;
+        var l;
         // populate draw pile
         for (var i = 0; i < o.decks; i++) {
-			// standard
+            // standard
             for (var s in o.suits) {
                 for (var r in o.ranks) {
-					l = this.cards.length;
-                    this.cards[l] = new $.fn.playingCards.card(r, o.ranks[r], s, o.suits[s]);
-
-					// debugging design mode (just laying out all the cards in order)
-					this.append(this.cards[l].getHTML());
+                    l = this.cards.length;
+                    this.cards[l] = new playingCards.card(r, o.ranks[r], s, o.suits[s]);
                 }
             }
-			// jokers
-			for (i=0;i<o.jokers;i++){
-					l = this.cards.length;
-					// suit will always be 1 or 2
-                    this.cards[l] = new $.fn.playingCards.card("N", o.jokerText, (i%2)+1, '');
-
-					// debugging design mode (just laying out all the cards in order)
-					this.append(this.cards[l].getHTML());
-			}
+            // jokers
+            for (i = 0; i < o.jokers; i++) {
+                l = this.cards.length;
+                // suit will always be 1 or 2
+                this.cards[l] = new playingCards.card("N", o.jokerText, (i % 2) + 1, '');
+            }
         }
-		/* demo flip*/
-		this.find('.playingCard').click(function(){
-			$(this).find('.front').toggle();
-		});
-        this.draw = function() {
-            return this.cards.length > 0 ? this.cards.shift() : null;
-        };
-        this.addCard = function(card) {
-            this.cards.push(card);
-        };
-        this.count = function() {
-            return this.cards.length;
-        };
-
-        return this;
     };
-	$.fn.playingCards.prototype.shuffle = function(n) {
+    /**
+     * draw a card
+     */
+    playingCards.prototype.draw = function() {
+        return this.cards.length > 0 ? this.cards.shift() : null;
+    };
+    playingCards.prototype.addCard = function(card) {
+        this.cards.push(card);
+    };
+    playingCards.prototype.count = function() {
+        return this.cards.length;
+    };
+    playingCards.prototype.shuffle = function(n) {
         var r;
         // random #
         var tmp;
@@ -72,11 +95,26 @@
             }
         }
     };
-    $.fn.playingCards.defaults = {
+	/*
+	 * requires jquery (currently)
+	 */
+	    playingCards.prototype.spread = function(dest) {
+			if(!this.conf.el && !dest){
+				return false;
+			}
+			var to = this.conf.el || dest;
+	        var l = this.cards.length;
+	        for (var i = 0; i < l; i++) {
+	            to.append(this.cards[i].getHTML());
+	        }
+	    };
+    playingCards.defaults = {
         "decks": 1,
-		"renderMode": 'css', // TODO: enable 'font' option -- loading cards.ttf
-		"jokers":2,
-		"jokerText":"Joker",
+        // TODO: enable 'font' option -- loading cards.ttf
+        "renderMode": 'css',
+		"startShuffled":true,
+        "jokers": 2,
+        "jokerText": "Joker",
         "ranks": {
             "2": "Two",
             "3": "Three",
@@ -99,6 +137,7 @@
             "H": "Hearts"
         }
     };
+
     /**
 	 * create a playing card
 	 * 
@@ -110,30 +149,31 @@
 	 * 
 	 * @return object The card object
 	 */
-    $.fn.playingCards.card = function(rank, rankString, suit, suitString, conf) {
+    playingCards.card = function(rank, rankString, suit, suitString, conf) {
         if (! (this instanceof arguments.callee)) {
-            return new arguments.callee(rank, rankString, suit, suitString);
+            return new arguments.callee(rank, rankString, suit, suitString, conf);
         }
 
-	    this.conf = $.extend({},$.fn.playingCards.card.defaults,conf);
-	
+        this.conf = objExtend(playingCards.card.defaults, conf);
+
         this.rank = rank;
         this.rankString = rankString;
         this.suit = suit;
         this.suitString = suitString;
         return this;
     };
-	$.fn.playingCards.card.defaults = {
-		"singleFace":false // false will use a different image for each suit/face, true will use diamond image for all
-	};
-    $.fn.playingCards.card.prototype.toString = function() {
-        // TODO: localize "of"
-        return this.suitString!=="" ? this.rankString + " of " + this.suitString : this.rankString;
+    playingCards.card.defaults = {
+        "singleFace": false
+        // false will use a different image for each suit/face, true will use diamond image for all
     };
-    $.fn.playingCards.card.prototype.getHTML = function() {
-		if(this.html){
-			return this.html;
-		}
+    playingCards.card.prototype.toString = function() {
+        // TODO: localize "of"
+        return this.suitString !== "" ? this.rankString + " of " + this.suitString: this.rankString;
+    };
+    playingCards.card.prototype.getHTML = function() {
+        if (this.html) {
+            return this.html;
+        }
         this.suitCode = "&nbsp;";
         this.colorCls = '';
         switch (this.suit) {
@@ -155,38 +195,36 @@
 
         // concatenating strings with "+" is slow, using array join is faster: http://code.google.com/speed/articles/optimizing-javascript.html
         // TODO: run perf test to be sure that in this case we are getting better perf in IE
-		var txt = this.rank;
-		if(this.rank==="N"){
-			txt = this.rankString.split('').join('<br />');
-		}
+        var txt = this.rank;
+        if (this.rank === "N") {
+            txt = this.rankString.split('').join('<br />');
+        }
         var strBuild = ['<div class="playingCard"><div class="front ', this.colorCls, '"><div class="corner">', txt, '<br />', this.suitCode, '</div>'];
         strBuild = strBuild.concat(this.buildIconHTML());
         strBuild = strBuild.concat('<div class="corner cornerBR flip">', txt, '<br />', this.suitCode, '</div></div></div>');
-		this.html = strBuild.join('');
+        this.html = strBuild.join('');
         return this.html;
     };
-    $.fn.playingCards.card.prototype.buildIconHTML = function() {
+    playingCards.card.prototype.buildIconHTML = function() {
         // TODO: could we optimize this with a for loop that breaks/continues to named positions?
         if (this.rank === "A") {
             return ['<div class="suit suit0">', this.suitCode, '</div>'];
         }
-        if (this.rank === "J" || this.rank === "Q" || this.rank === "K"  || this.rank === "N") {
-			var n = 'D';
-			if(!this.conf.singleFace){
-				n = this.suit;
-			}
+        if (this.rank === "J" || this.rank === "Q" || this.rank === "K" || this.rank === "N") {
+            var n = 'D';
+            if (!this.conf.singleFace) {
+                n = this.suit;
+            }
             return [
             '<div class="suit A1">', this.suitCode, '</div>',
-            '<img class="suit ', this.rank, ' face" src="img/',this.rank,n,'.gif"/>',
+            '<img class="suit ', this.rank, ' face" src="img/', this.rank, n, '.gif"/>',
             '<div class="suit C5 flip">', this.suitCode, '</div>'
             ];
         }
         var ret = [];
         var list = ['4', '5', '6', '7', '8', '9', '10'];
         // all of these will have A1, A5, C1, C5 icons
-        // using jquery's inArray(). If we make a non-jquery version,
-        // we will have to include an in_array proto on array (or change the way this is built)
-        if ($.inArray(this.rank, list)!==-1) {
+        if (list.indexOf(this.rank) !== -1) {
             ret = ret.concat([
             '<div class="suit A1">', this.suitCode, '</div>',
             '<div class="suit A5 flip">', this.suitCode, '</div>',
@@ -195,39 +233,39 @@
             ]);
         }
         list = ['2', '3'];
-        if ($.inArray(this.rank, list)!==-1) {
+        if (list.indexOf(this.rank) !== -1) {
             ret = ret.concat([
             '<div class="suit B1">', this.suitCode, '</div>',
             '<div class="suit B5 flip">', this.suitCode, '</div>'
             ]);
         }
         list = ['3', '5', '9'];
-        if ($.inArray(this.rank, list)!==-1) {
+        if (list.indexOf(this.rank) !== -1) {
             ret = ret.concat([
             '<div class="suit B3">', this.suitCode, '</div>'
             ]);
         }
         list = ['6', '7', '8'];
-        if ($.inArray(this.rank, list)!==-1) {
+        if (list.indexOf(this.rank) !== -1) {
             ret = ret.concat([
             '<div class="suit A3">', this.suitCode, '</div>',
             '<div class="suit C3">', this.suitCode, '</div>'
             ]);
         }
         list = ['7', '8', '10'];
-        if ($.inArray(this.rank, list)!==-1) {
+        if (list.indexOf(this.rank) !== -1) {
             ret = ret.concat([
             '<div class="suit B2">', this.suitCode, '</div>'
             ]);
         }
         list = ['8', '10'];
-        if ($.inArray(this.rank, list)!==-1) {
+        if (list.indexOf(this.rank) !== -1) {
             ret = ret.concat([
             '<div class="suit B4 flip">', this.suitCode, '</div>'
             ]);
         }
         list = ['9', '10'];
-        if ($.inArray(this.rank, list)!==-1) {
+        if (list.indexOf(this.rank) !== -1) {
             ret = ret.concat([
             '<div class="suit A2">', this.suitCode, '</div>',
             '<div class="suit A4 flip">', this.suitCode, '</div>',
@@ -237,4 +275,17 @@
         }
         return ret;
     };
-})(jQuery);
+
+    /**
+	 * Simple object extend to override default settings
+	 */
+    function objExtend(o, ex) {
+        if (!ex) {
+            return o;
+        }
+        for (p in ex) {
+            o[p] = ex[p];
+        }
+        return o;
+    }
+})(typeof(jQuery) !== 'undefined' ? jQuery: function(){});
